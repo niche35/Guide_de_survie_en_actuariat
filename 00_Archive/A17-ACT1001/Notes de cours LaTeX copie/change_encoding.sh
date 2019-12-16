82e102daea5c8bd39bf229d0135f6d044caa74fd
Gabriel Crépeault-Cauchon <gabrielcrepeault@gmail.com>
2019-12-16 12:59:39 -0500

    Début cheatsheet STT2200

---
+++ 00_Archive/A17-ACT1001/Notes de cours LaTeX copie/change_encoding.sh
@@ -0,0 +1,19 @@
+# Chemin d'accès vers le répertoire
+# cd BLABLABLA
+
+# Loop pour avoir l'info de tous les fichiers .tex
+for FICHIER in *.tex
+do
+  echo Vérification pour le fichier $FICHIER
+  file -I $FICHIER
+done
+
+# Loop pour modifier l'encoding des fichiers
+for FICHIER in *.tex
+do
+  iconv -f ISO-8859-1 -t UTF-8//TRANSLIT "$FICHIER" -o "$FICHIER"
+  #iconv -f ISO-8859-1 -t UTF-8 $FICHIER  $FICHIER
+done
+
+## Conversion des différents fichiers .tex (merci à David B.!)
+# iconv -f ISO-8859-1 -t UTF-8//TRANSLIT nom_fichier_a_convertir -o name_after_conversion
